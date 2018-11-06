b0590400debaa8c8856b4d8301c4129245b9a76d
Veraprot <vera.protopopova@gmail.com>
2018-11-06 11:35:59 -0500

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,3 +1,4 @@
+
 class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
@@ -9,8 +10,24 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build(quantity: 1)
+    @recipe.ingredients.build(quantity: 1)
   end
 
   def create
+    @recipe = Recipe.create(recipe_params)
+    redirect_to @recipe
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(
+      :title,
+      ingredients_attributes: [
+        :name,
+        :quantity
+      ] 
+    )
   end
 end
